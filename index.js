651b1b2ad947393472258506600c709c4779e238
irinove <45695615+irinove@users.noreply.github.com>
2019-03-29 00:50:04 +0600

    play sounds, js objects

--- index.js
+++ index.js
@@ -2,8 +2,34 @@ var numbeOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i<numbeOfDrumButtons; i++) {
 
-document.querySelectorAll(".drum")[i].addEventListener("click", function ()  {
-	alert("I got Clicked");
-}); 
+document.querySelectorAll (".drum")[i].addEventListener("click", function ()  {
+	// alert("I got Clicked");
+	
+	var audio = new Audio("sounds/tom-1.mp3");
+	  audio.play(); 
+ 		
+ 	 // this.style.color = "white";	
+ 	 	
+	
+});
 
 }
+
+
+
+// var audio = new Audio('sounds/tom-1.mp3');
+	// audio.play(); 
+
+// var housekeeper1 = new HouseKeeper 
+
+function HouseKeeper (yearsOfExperience, name, cleanningExperties) {
+	this.yearsOfExperience = yearsOfExperience;
+	this.name = name;
+	this.cleanningExperties = cleanningExperties;
+}
+
+var housekeeper1 new HouseKeeper(9, "tom", ["lobby"," bedroom"]);
+console.log(housekeeper1.name);
+console.log(housekeeper1.yearsOfExperience);
+
+ar housekeeper2 new HouseKeeper(19, "tomy", ["lobby",
