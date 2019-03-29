5e8fdf34a3dbb237bcc84ba170c606cef9cc4adf
irinove <45695615+irinove@users.noreply.github.com>
2019-03-29 21:13:47 +0600

    switch statements in js

--- index.js
+++ index.js
@@ -5,10 +5,55 @@ for (var i = 0; i<numbeOfDrumButtons; i++) {
 document.querySelectorAll (".drum")[i].addEventListener("click", function ()  {
 	// alert("I got Clicked");
 	
-	var audio = new Audio("sounds/tom-1.mp3");
-	  audio.play(); 
+	 // var audio = new Audio('sounds/tom-1.mp3');
+	 // audio.play();  
  		
- 	 // this.style.color = "white";	
+ 	  // this.style.color = "white";
+
+ 	  var buttonInnerHTML = this.innerHTML;   
+
+ 	   switch (buttonInnerHTML) {
+ 	   	case "w":
+ 	   		 var tom1 = new Audio('sounds/tom-1.mp3');
+	          tom1.play();  
+ 		
+            break;
+
+        case "a":
+        	var tom2 = new Audio('sounds/tom-2.mp3');
+	         tom2.play();  
+	        break;
+
+	    case "s":
+        	var tom3 = new Audio('sounds/tom-3.mp3');
+	         tom3.play();  
+	       break;
+
+	    case "d":
+        	var tom4 = new Audio('sounds/tom-4.mp3');
+	         tom4.play();  
+	       break;
+
+	    case "j":
+        	var snare = new Audio('sounds/snare.mp3');
+	         snare.play();  
+	       break;
+
+	    case "k":
+        	var crash = new Audio('sounds/crash.mp3');
+	         crash.play();  
+	       break;
+
+	    case "l":
+        	var kick = new Audio('sounds/kick-bass.mp3');
+	         kick.play();  
+	       break;
+
+
+
+          default: console.log(buttonInnerHTML);  
+ 	   }
+
  	 	
 	
 });
@@ -16,20 +61,4 @@ document.querySelectorAll (".drum")[i].addEventListener("click", function ()  {
 }
 
 
-
-// var audio = new Audio('sounds/tom-1.mp3');
-	// audio.play(); 
-
-// var housekeeper1 = new HouseKeeper 
-
-function HouseKeeper (yearsOfExperience, name, cleanningExperties) {
-	this.yearsOfExperience = yearsOfExperience;
-	this.name = name;
-	this.cleanningExperties = cleanningExperties;
-}
-
-var housekeeper1 new HouseKeeper(9, "tom", ["lobby"," bedroom"]);
-console.log(housekeeper1.name);
-console.log(housekeeper1.yearsOfExperience);
-
-ar housekeeper2 new HouseKeeper(19, "tomy", ["lobby",
+ 
